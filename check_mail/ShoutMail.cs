f268fe92b959fd50a0e090b5c659f831c153cc4a
bhagi <bhargavak37@gmail.com>
2017-07-18 12:30:50 +0530

    Removed commented out code

--- check_mail/ShoutMail.cs
+++ check_mail/ShoutMail.cs
@@ -40,62 +40,8 @@ namespace check_mail
             //Imap_Check_Msg();
 
         }
-        //public static void ReadImap()
-        //{
-        //    var mailRepository = new MailRepository(
-        //                            "imap.gmail.com",
-        //                            993,
-        //                            true,
-        //                            "",
-        //                            ""
-        //                        );
-
-        //    var emailList = mailRepository.GetAllMails("inbox");
-
-        //    foreach (Message email in emailList)
-        //    {
-        //        Console.WriteLine("<p>{0}: {1}</p><p>{2}</p>", email.From, email.Subject, email.BodyHtml.Text);
-        //        Console.Read();
-        //        if (email.Attachments.Count > 0)
-        //        {
-        //            foreach (MimePart attachment in email.Attachments)
-        //            {
-        //                Console.WriteLine("<p>Attachment: {0} {1}</p>", attachment.ContentBase, attachment.ContentType.MimeType);
-        //            }
-        //        }
-        //    }
-        //}
-        //static void Imap_Check_Msg() {
-
-        //    using (var client = new MailKit.Net.Imap.ImapClient())
-        //    {
-        //        // For demo-purposes, accept all SSL certificates
-        //        client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-
-        //        client.Connect("imap.gmail.com", 993, true);
-
-        //        // Note: since we don't have an OAuth2 token, disable
-        //        // the XOAUTH2 authentication mechanism.
-        //        client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-        //        client.Authenticate("", "");
-        //       // The Inbox folder is always available on all IMAP servers...
-        //       var inbox = client.Inbox;
-        //       inbox.Open(FolderAccess.ReadOnly);
-
-        //      Console.WriteLine("Total messages: {0}", inbox.Count);
-        //        Console.WriteLine("Recent messages: {0}", inbox.Recent);
-
-        //        for (int i = 0; i < inbox.Count; i++)
-        //        {
-        //            var message = inbox.GetMessage(i);
-        //            Console.WriteLine("Subject: {0}", message.Subject);
-        //            //MessageBox.Show(message.Subject);
-        //        }
-
-        //        client.Disconnect(true);
-        //    }
-        //}
+     
+       
         
         static IMailFolder get_mail()
         {
